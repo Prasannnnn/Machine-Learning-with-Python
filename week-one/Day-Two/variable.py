dec59a0fe9bad23ef3fdd396422a8ea1b6665072
Prasannnnn <prasannamb4362@gmail.com>
2024-12-07 17:59:32 +0530

    variable

---
+++ week-one/Day-Two/variable.py
@@ -0,0 +1,71 @@
+#Variables
+#variables are containers for storing data values
+# masala_box = "Masala Podi"
+# print("thambi poi podi eduthutu vaa",masala_box)
+# ctrl + /
+#creating Variables
+"""
+python has no commands for declaring a variable
+a variable is created that moment you first assign
+
+"""
+# b = 10
+# c = 20
+# print(b+c)
+
+# d = "ten"
+# print(d)
+# d = 10
+# print("Ben",d)
+
+
+#Casting
+e = str(5)
+f = float(100)
+g = int(3.8)
+print("Str:",e,
+      "Float:",f,
+      "integer:",g,)
+
+#float --> Decimal Points ==> 0.5 or 5.0
+
+#What is a correct way to declare a Python variable?
+"""
+var x = 5
+#x = 5
+$x = 5
+x = 5
+"""
+# X=5 is an correct answer
+
+my_name = "Ashwath"
+my_age = 23
+my_college = "SIST"
+my_native = "Bangalore"
+print(my_name)
+
+#abc = "asd"
+#a variable name must start with a letter or underscore character
+
+# a variable name cannot start with a number
+# a variable name contain only alpha-numeric values and characters
+# (0-9) a9-z9
+# variable names are case sensitive 
+age =23
+print(age) 
+# a variable name cannot be any of python keywords
+
+# Assign multiple values
+
+x , y, z = "Biriyani","Curd Rice", "Idly"
+
+print(y)
+print(z)
+print(x)
+
+#unpack a collection
+
+meals = ["Non-veg","veg","vegan"] #-> lists
+x,y,z = meals
+print(x)
+
