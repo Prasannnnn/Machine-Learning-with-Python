6d8b86a74d2cb4460304b99f6958b30dfe68c3f4
Prasannnnn <prasannamb4362@gmail.com>
2024-12-09 18:56:31 +0530

    day-3

---
+++ week-two/day-three/data_types.py
@@ -0,0 +1,54 @@
+#Data types
+'''
+Python Data types
+integer -> int is an data type it contains 0-100,
+floating point -> float it contains 0.5 - 100.99
+string -> str -> "Hello World"
+boolean -> bool -> True or False
+list -> [] -> ["Popcorn Chicken", "Burger", "Double chicken Burger"]
+tuple -> () -> ("Popcorn Chicken", "Burger", "Double chicken Burger")
+dictionaries -> {Name : "Velu", Age : 23}
+set -> {"Popcorn Chicken", "Burger", "Double chicken Burger"}
+'''
+#integer
+# i = 65
+# print("this is an integer ",i)
+# print(type(i))
+# #float
+# j = 6.5
+# print("this is an float",j)
+# print(type(j))
+# #string
+# k = "Only thing that could make this day better is ice cream"
+# print("My Quotes is ",k)
+# print(type(k))
+# #boolean
+# l = False
+# print(l)
+# print(type(l))
+#data types
+#list,tuple,set and dictionary
+
+#lists
+m = ["Jackie Chan","Dragon Booster","Gloriavin Veedu", "Chin Chan","Pokeman","Ninja Hattori", "Ben10"]
+#index
+#      0                   1                 2             3            4          5             6
+print(m)
+#data type
+print(type(m))
+#length of data
+print(len(m))
+#Access lists items
+print(m[2])
+#change lists items
+m[6] = "DANDANDAN"
+print(m)
+print(len(m))
+#multi change lists items
+print(m[2:])
+print(m[:6])
+print(m[2:6])
+#m[2:3]
+#left side starting
+#right side stop before value
+print(len(m))
