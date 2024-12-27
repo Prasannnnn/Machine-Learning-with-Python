3fffa47babc658daa84aa4f76dc63cac097b4c78
prasannnnn <prasannamb4362@gmial.com>
2024-12-27 16:37:37 +0530

    functions

---
+++ week-four/functions.py
@@ -0,0 +1,59 @@
+'''
+a function is a block of code
+
+
+'''
+
+# def morn():
+#     print("Good Morning")
+
+# def add(a,b):
+#     return a+b
+
+
+# a = float(input("Enter the value"))
+# b = float(input("Enter the value"))
+# print("Enter the Arithmetic Operators")
+# c = input("Enter the Operators")
+# if c == "+":
+#     print(add(a,b))
+
+
+'''
+user two inputs
+operator user input
+
+a = 10
+b = 5 
+operators = +
+print(add(a,b))
+50
+
+'''
+
+def fname(a):
+    print(a+"Dhoni")
+
+fname("Thala")
+fname("7 Reason of ")
+fname("Captain")
+fname("My idol is ")
+
+# a ="Good Morning"
+# b = "Good Evening"
+# c = "Good Night"
+# print( a,"by steve" )
+# print(b,"by steve")
+
+
+a = 10
+b = 5
+
+a = a+b #15
+b = a-b #10
+a = a-b #5
+
+#swap program
+print(a)#5
+print(b)#10
+
---
+++ week-four/identity.py
@@ -0,0 +1,7 @@
+# is 
+# isnot
+
+fruits = ["apple","banana"]
+fruit = ["apple","banana"]
+
+print(fruits is fruit)
